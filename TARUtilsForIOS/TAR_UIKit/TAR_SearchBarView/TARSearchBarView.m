570c31fd407cbc217840e3ebdf6603c44d68110f
touaiei <touaiei@163.com>
2018-03-08 14:05:14 +0800

    0.2.8
    修改 TARSearchBarView.m

--- TARUtilsForIOS/TAR_UIKit/TAR_SearchBarView/TARSearchBarView.m
+++ TARUtilsForIOS/TAR_UIKit/TAR_SearchBarView/TARSearchBarView.m
@@ -14,6 +14,7 @@
 @interface TARSearchBarView()
 <UITextFieldDelegate>
 {
+    UIImageView *_searchImageView;
     
 }
 @end
@@ -35,10 +36,10 @@
     self = [super initWithFrame:frame];
     if (self) {
         [self initialize];
-
+        
         // 提前在Xcode上设置图片中间拉伸
         self.background = [UIImage imageNamed:@"searchbar_textfield_background"];
-
+        
         [self setSearchImageView];
         
     }
@@ -73,19 +74,19 @@
 -(void)setSearchImageView
 {
     UIView *searchBGView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.bounds.size.height, self.bounds.size.height)];
-//    searchBGView.layer.borderWidth = 1;
-//    searchBGView.layer.borderColor = [UIColor yellowColor].CGColor;
+    //    searchBGView.layer.borderWidth = 1;
+    //    searchBGView.layer.borderColor = [UIColor yellowColor].CGColor;
     UITapGestureRecognizer *searchButtonTap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(searchButtonClick:)];
     [searchBGView addGestureRecognizer:searchButtonTap];
     
     // 通过init初始化的控件大多都没有尺寸
     CGFloat searchImageView_W_H = 20;
-    UIImageView *searchImageView = [[UIImageView alloc] initWithFrame:CGRectMake((searchBGView.width-searchImageView_W_H)/2.0, (searchBGView.height-searchImageView_W_H)/2.0, searchImageView_W_H, searchImageView_W_H)];
-    //        searchIcon.image = [UIImage imageNamed:@"searchbar_textfield_search_icon"];
-    searchImageView.image = [UIImage imageNamed:@"home_Pay_mechanism_search_ic"];
+    _searchImageView = [[UIImageView alloc] initWithFrame:CGRectMake((searchBGView.width-searchImageView_W_H)/2.0, (searchBGView.height-searchImageView_W_H)/2.0, searchImageView_W_H, searchImageView_W_H)];
+    //        _searchImageView.image = [UIImage imageNamed:@"searchbar_textfield_search_icon"];
+    _searchImageView.image = [UIImage imageNamed:@"home_Pay_mechanism_search_ic"];
     // contentMode：default is UIViewContentModeScaleToFill，要设置为UIViewContentModeCenter：使图片居中，防止图片填充整个imageView
     //        searchIcon.contentMode = UIViewContentModeCenter;
-    [searchBGView addSubview:searchImageView];
+    [searchBGView addSubview:_searchImageView];
     
     self.leftView.layer.borderWidth = 1;
     self.leftView.layer.borderColor = [UIColor redColor].CGColor;
@@ -104,6 +105,12 @@
 {
     [self setValue:placeholderFont forKeyPath:@"_placeholderLabel.font"];//设置占位符文字大小
 }
+
+-(void)setSearchImage:(UIImage *)searchImage
+{
+    _searchImageView.image = searchImage;
+}
+
 
 /**
  搜索按钮点击事件
@@ -134,3 +141,4 @@
 }
 
 @end
+
